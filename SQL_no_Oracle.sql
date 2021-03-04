f534fff8468a753cc9fe17e2f5487383d5f9f537
Borges Gaya Neto <borgesgaya@gmail.com>
2021-03-04 10:54:48 -0300

    Update SQL_no_Oracle.sql

--- SQL_no_Oracle.sql
+++ SQL_no_Oracle.sql
@@ -1,1 +1,5 @@
-teste
+--SELECT CONTINUA "NORMAL"
+
+	select idturma,turmanome,count(idade) as contador from tabela  where nome like 'A%' and Idade between 10 and 15 group by idturma,turmanome order by nome desc;
+  
+  --Vai ter mais.
